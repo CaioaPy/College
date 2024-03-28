d19ad64f964ffee24f552152aeb7353a4cc49fe9
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-03-28 10:25:52 -0300

    Update test.java

--- test.java
+++ test.java
@@ -5,15 +5,15 @@ public class test {
 Scanner leitor = new Scanner(System.in);
 int a = 12;
 int b = 23;
-int resultado = 0;
 System.out.println("Hello, world! How are you?");
 String State = leitor.nextLine();
-System.out.println("Good to know you're " + State + "!\nif a = 12 and b = 23 how much is a + b?");
+System.out.println("Good to know you're " + State + "!\nif a = 12 and b = 23 then how much is a + b?");
 String resultado = leitor.nextLine();
-if (resultado == 35) {
-    System.out.println("You're right!"); }
+System.out.println(resultado + " huh? so...");
+if (resultado == "35") {
+    System.out.println("You're right! The result is " + resultado); }
 else {
-    System.out.println(resultado + " is not correct!"); }
+    System.out.println("That is wrong!"); }
 
     }
 }
