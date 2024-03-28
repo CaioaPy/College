a5c9b01cf3e4d7e1186e23123b9ce8f57f4dc151
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-03-28 09:43:38 -0300

    Update geral.java

--- geral.java
+++ geral.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-    Scanner leitor = new Scanner(System.in);
-    a = 12;
-    b = 23;
-    resultado = 0;
-    System.out.println("Hello, world! How are you?");
-    String State = leitor.nextLine();
-    System.out.println("Good to know you're " + State + "!\nif a = 12 and b = 23 how much is a + b?");
-    String Resultado = leitor.nextLine();
-    If (resultado = 35); {
-        System.out.println("You're right!"); }
-    else {
-        System.out.println(resultado + "is not correct!"); }
+Scanner leitor = new Scanner(System.in);
+int a = 12;
+int b = 23;
+int resultado = 0;
+System.out.println("Hello, world! How are you?");
+String State = leitor.nextLine();
+System.out.println("Good to know you're " + State + "!\nif a = 12 and b = 23 how much is a + b?");
+String resultado = leitor.nextLine();
+if (resultado == 35) {
+    System.out.println("You're right!"); }
+else {
+    System.out.println(resultado + " is not correct!"); }
 
     }
 }
