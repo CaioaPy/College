24a342dc55ddc0cb687018cdead5f4583f1f5f29
yatomikazuchi <caioevilabner@gmail.com>
2024-03-31 12:09:09 -0300

    general modified

--- test.java
+++ test.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-Scanner leitor = new Scanner(System.in);
-System.out.println("Hello, world! How are you?");
-String State = leitor.nextLine();
-System.out.println("Good to know you're " + State + "!\nif a = 12 and b = 23 then how much is a + b?");
-String resultado = leitor.nextLine();
-System.out.println(resultado + " huh? so...");
-if (resultado == "35") {
-    System.out.println("You're right! The result is " + resultado); }
-else {
-    System.out.println("That is wrong!"); }
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Hello, world! How are you?");
+        String State = leitor.nextLine();
+        System.out.println("Good to know you're " + State + "!\nif a = 12 and b = 23 then how much is a + b?");
+        String resultado = leitor.nextLine();
+        System.out.println(resultado + " huh? so...");
+        if (resultado == "35") {
+            System.out.println("You're right! The result is " + resultado);
+        } else {
+            System.out.println("That is wrong!");
+        }
 
     }
 }
