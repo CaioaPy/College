b881e49c1edc57608d7c80f5176688db696ea660
yatomikazuchi <caioevilabner@gmail.com>
2024-03-31 12:11:36 -0300

    file for just one of the tasks

---
+++ HM1/section.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class section {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int AP = leitor.nextInt();
+        System.out.println("Digite a altura da parede:");
+        int LP = leitor.nextInt();
+        System.out.println("Digite a largura da parede:");
+        int AA = leitor.nextInt();
+        System.out.println("Digite a altura do azulejo:");
+        int LA = leitor.nextInt();
+        System.out.println("Digite a largura do azulejo:");
+        int AreaP = AP * LP;
+        int AreaA = AA * LA;
+        int QuantidadeA = AreaP / AreaA;
+        System.out.println("Você precisará de " + QuantidadeA + "Azulejos");
+    }
+}
