999f68f7e2ea52032ac26cae2c0282404cadf460
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-04-04 09:13:07 -0300

    Update section.java

--- HM1/section.java
+++ HM1/section.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
-public class Exercicio10 {
+public class Exercicio5 {
     public static void main(String[] args) {
-            double A;
-            double B;
-            double C;
+            double IMC;
+            double h;
+            double m;
+            double h2;
         Scanner leitor = new Scanner(System.in);
-            System.out.println("Insira o valor de A:");
-            A = leitor.nextDouble();
-            System.out.println("Insira o valor de B:");
-            B = leitor.nextDouble();
-            C = A;
-            A = B;
-            B = C;
-            System.out.println("Os novos valores são A = " + A + " e B = " + B );
+            System.out.println("Insira o valor de m (massa):");
+            m = leitor.nextDouble();
+            System.out.println("Insira o valor de h (altura):");
+            h = leitor.nextDouble();
+            h2 = h*h;
+            IMC = m / h2;
+            System.out.println("O IMC é de: " + IMC);
     
     }
 }
