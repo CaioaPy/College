52bcc5b19c5915f2a2e8c5ae6e9491e276987430
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-04-04 08:58:57 -0300

    Update atividade2.java

--- HM1/atividade2.java
+++ HM1/atividade2.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 
-public class atividade2 {
+public class Exercicio1 {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
         /*
          * USD = BRL 2,40
          * 1)
          */
-
+        Scanner leitor = new Scanner(System.in);
         System.out.println("Digite a quantidade de reais a ser convertida para dólares:");
         int BRL = leitor.nextInt();
         double convertido = BRL / 2.40;
@@ -18,15 +17,24 @@ public class atividade2 {
          * 2)
          */
 
+public class Exercicio2 {
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
         System.out.println("Digite a quantidade de dólares a ser convertida para reais:");
         int USD = leitor.nextInt();
         double convertido2 = USD * 2.40;
         System.out.println("A quantidade em reais é: " + convertido2);
 
+    }
+}
         /*
          * 3)
          */
-
+public class Exercicio3 {
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
         double AP = leitor.nextDouble();
         System.out.println("Digite a altura da parede:");
         double LP = leitor.nextDouble();
@@ -40,9 +48,33 @@ public class atividade2 {
         double QuantidadeA = AreaP / AreaA;
         System.out.println("Você precisará de " + QuantidadeA + " Azulejos");
 
+    }
+}
+
         /*
          * 4)
          */
 
+public class Exercicio4 {
+    public static void main(String[] args) {
+        
+        double ladoA=0;
+        double ladoB;
+        double area;
+        double perimetro;
+    Scanner leitor = new Scanner(System.in);
+        System.out.println("Insira o valor do lado A:");
+        ladoA = leitor.nextDouble();
+        System.out.println("Insira o valor do lado B:");
+        ladoB = leitor.nextDouble();
+        area = ladoA * ladoB;
+        perimetro = 2*ladoA + 2*ladoB;
+        System.out.println("A area é = " + area);
+        System.out.println("E o perímetro é = " + perimetro);
+
+
+    }
+}
+        
     }
 }
