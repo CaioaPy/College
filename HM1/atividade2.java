2eee9e9fd41f01ba28b04bd62543b52d02c0064c
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-04-04 09:12:49 -0300

    Update atividade2.java

--- HM1/atividade2.java
+++ HM1/atividade2.java
@@ -79,7 +79,30 @@ public class Exercicio4 {
 
     }
 }
-        
+
+            /*
+            *IMC = m/h²
+            *10)
+            */
+import java.util.Scanner;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+            double IMC;
+            double h;
+            double m;
+            double h2;
+        Scanner leitor = new Scanner(System.in);
+            System.out.println("Insira o valor de m (massa):");
+            m = leitor.nextDouble();
+            System.out.println("Insira o valor de h (altura):");
+            h = leitor.nextDouble();
+            h2 = h*h;
+            IMC = m / h2;
+            System.out.println("O IMC é de: " + IMC);
+    
+    }
+}
         /*
         *10)
         */
