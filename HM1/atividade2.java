4ee83c122e8951484e9c2bb928dc62e514b850cc
yatomikazuchi <caioevilabner@gmail.com>
2024-04-05 22:07:18 -0300

    updated

--- HM1/atividade2.java
+++ HM1/atividade2.java
@@ -219,7 +219,7 @@ public class Exercicio9 {
         /*
         *10)
         */
-    import java.util.Scanner;
+import java.util.Scanner;
 
 public class Exercicio10 {
     public static void main(String[] args) {
@@ -236,5 +236,27 @@ public class Exercicio10 {
             B = C;
             System.out.println("Os novos valores são A = " + A + " e B = " + B );
     
+    }
+}
+
+        /*
+         * 
+         * 11)
+         */
+import java.util.Scanner;
+
+public class Exercicio11 {
+    public static void main(String[] args) {
+        double km;
+        double tempo;
+        double velocidadeM;
+        Scanner leitor = new Scanner(System.in);
+            System.out.println("Insira a distancia total (km) do percurso:");
+            km = leitor.nextDouble();
+            System.out.println("Insira o tempo total no percurso (minutos): ");
+            tempo = leitor.nextDouble();
+            km = km * 60;
+            velocidadeM = km / tempo;
+            System.out.println("A velocidade média é de: "+ velocidadeM);
     }
 }
