680e86a492cfcbf4e55f987930d4feb70bc11885
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-04-11 09:28:32 -0300

    Update switch.java

--- switch.java
+++ switch.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+public class ww{
+    public static void main(String []args){
+        String po = "o";
 
+    switch(po) {
+        case "oi":
+            System.out.println("oi ww");
+            break;
+        case "nao":
+            System.out.println("wwww");
+            break;
+        default:
+            System.out.println("invalido, responda apenas com oi/nao");
+            break;
+
+        }
+    }
+}
