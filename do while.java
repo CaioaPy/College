44ae30bca16e877d383295dc7c5f02020a8859dc
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-04-11 10:58:29 -0300

    Update do while.java

--- do while.java
+++ do while.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
 
+public class ww{
+
+    public static void main(String []args){
+        int numero = 10;
+        int n = 5;
+        do{
+            numero = numero * 2;
+            n --;
+            System.out.println(numero);
+                } while (n >= 0);
+    }
+}
