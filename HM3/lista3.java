19175872aed61308385afce0dbdcbdfb99f4c73a
yatomikazuchi <caioevilabner@gmail.com>
2024-04-30 22:51:23 -0300

    exercise 2

--- HM3/lista3.java
+++ HM3/lista3.java
@@ -1,6 +1,6 @@
 package HM3;
 
-public class lista3 {
+public class exercicio1 {
     public static void main(String[] args) {
         for (int n = 0; n < 20; n = n + 1){
             int n2;
@@ -14,16 +14,15 @@ public class lista3 {
     }
 }
 
-import Scanner;
-
-public class lista3 {
+public class exercicio2 {
     public static void main(String[] args) {
-        
-        
-        Scanner leitor = new Scanner(System.in);
-
-
-        
+        for (int n = 0; n < 100; n = n + 1) {
+            int soma = 0;
+            for (int n2 = 0; n2 <= n; n2 = n2 + 1) {
+                soma += n2;
+            }
+            System.out.println(soma);
+        }
     }
 }
 
