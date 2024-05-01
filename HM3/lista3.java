8d952a404f51d0c3538be5dc4f2612f07c85ba0e
yatomikazuchi <caioevilabner@gmail.com>
2024-04-30 22:37:39 -0300

    exercise 1

--- HM3/lista3.java
+++ HM3/lista3.java
@@ -1,9 +1,154 @@
 package HM3;
 
+public class lista3 {
+    public static void main(String[] args) {
+        for (int n = 0; n < 20; n = n + 1){
+            int n2;
+            n2 = n;
+            if (n2 % 2 == 0)
+                System.out.println(n2);
+            else {}
+            
+            
+        }
+    }
+}
+
 import Scanner;
 
 public class lista3 {
     public static void main(String[] args) {
+        
+        
+        Scanner leitor = new Scanner(System.in);
+
+
+        
+    }
+}
+
+import Scanner;
+
+public class lista3 {
+    public static void main(String[] args) {
+        
+        
+        Scanner leitor = new Scanner(System.in);
+
+
+        
+    }
+}
+
+import Scanner;
+
+public class lista3 {
+    public static void main(String[] args) {
+        
+        
+        Scanner leitor = new Scanner(System.in);
+
+
+        
+    }
+}
+
+import Scanner;
+
+public class lista3 {
+    public static void main(String[] args) {
+        
+        
+        Scanner leitor = new Scanner(System.in);
+
+
+        
+    }
+}
+
+import Scanner;
+
+public class lista3 {
+    public static void main(String[] args) {
+        
+        
+        Scanner leitor = new Scanner(System.in);
+
+
+        
+    }
+}
+
+import Scanner;
+
+public class lista3 {
+    public static void main(String[] args) {
+        
+        
+        Scanner leitor = new Scanner(System.in);
+
+
+        
+    }
+}
+
+import Scanner;
+
+public class lista3 {
+    public static void main(String[] args) {
+        
+        
+        Scanner leitor = new Scanner(System.in);
+
+
+        
+    }
+}
+
+import Scanner;
+
+public class lista3 {
+    public static void main(String[] args) {
+        
+        
+        Scanner leitor = new Scanner(System.in);
+
+
+        
+    }
+}
+
+import Scanner;
+
+public class lista3 {
+    public static void main(String[] args) {
+        
+        
+        Scanner leitor = new Scanner(System.in);
+
+
+        
+    }
+}
+
+import Scanner;
+
+public class lista3 {
+    public static void main(String[] args) {
+        
+        
+        Scanner leitor = new Scanner(System.in);
+
+
+        
+    }
+}
+
+import Scanner;
+
+public class lista3 {
+    public static void main(String[] args) {
+
         Scanner leitor = new Scanner(System.in);
 
     }
