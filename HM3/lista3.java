489976322166453bab6cf6cc2ef7a80de9d9f5c3
yatomikazuchi <caioevilabner@gmail.com>
2024-04-30 23:02:05 -0300

    exercise 3

--- HM3/lista3.java
+++ HM3/lista3.java
@@ -2,7 +2,7 @@ package HM3;
 
 public class exercicio1 {
     public static void main(String[] args) {
-        for (int n = 0; n < 20; n = n + 1){
+        for (int n = 0; n < 20; n++){
             int n2;
             n2 = n;
             if (n2 % 2 == 0)
@@ -16,9 +16,9 @@ public class exercicio1 {
 
 public class exercicio2 {
     public static void main(String[] args) {
-        for (int n = 0; n < 100; n = n + 1) {
+        for (int n = 0; n < 100; n++) {
             int soma = 0;
-            for (int n2 = 0; n2 <= n; n2 = n2 + 1) {
+            for (int n2 = 0; n2 <= n; n2++) {
                 soma += n2;
             }
             System.out.println(soma);
@@ -26,16 +26,20 @@ public class exercicio2 {
     }
 }
 
-import Scanner;
+import java.util.Scanner;
 
-public class lista3 {
+public class exercicio3 {
     public static void main(String[] args) {
-        
-        
+        int x;
         Scanner leitor = new Scanner(System.in);
-
-
-        
+        System.out.println("Insira o número desejado:");
+        x = leitor.nextInt();
+        for (int n = 0; n <= 10; n++;){
+            int x1;
+            x1 = x;
+            x1 = x1 * n;
+            System.out.println(x + " X " + n + " = " + x1);
+        }
     }
 }
 
