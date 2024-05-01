1c93cef821f8b945c40a9e0c3a4b413002e3bcb4
yatomikazuchi <caioevilabner@gmail.com>
2024-05-01 00:15:59 -0300

    exercise 8

--- HM3/lista3.java
+++ HM3/lista3.java
@@ -100,16 +100,19 @@ public class exercicio7 {
     }
 }
 
-import Scanner;
-
-public class lista3 {
+public class exercicio8 {
     public static void main(String[] args) {
-        
-        
-        Scanner leitor = new Scanner(System.in);
-
-
-        
+        int termo = 15;
+        int n1 = 1;
+        int n2 = 1;
+        int n3;
+        System.out.println(n1 + "\n" + n2);
+        for (int n = 2; n < termo; n++) {
+            n3 = n1 + n2;
+            System.out.println(n3);
+            n1 = n2;
+            n2 = n3;
+        }
     }
 }
 
