bb3cdedc98ca3a2a6ace0d95fb22e89a11980752
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-05-02 09:35:02 -0300

    Update Length.java

--- commands/Length.java
+++ commands/Length.java
@@ -1,1 +1,8 @@
-
+public class Length {
+	
+    public static void main(String[] args) {
+        String aa = "e5ruhtrei23u4324iuhr";
+        System.out.println(aa.length());
+    }
+}
+//output 20
