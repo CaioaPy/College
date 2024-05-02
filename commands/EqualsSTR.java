fde029a4a8b345d033ca5009dcb07acd4c96a998
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-05-02 09:33:21 -0300

    Update and rename EqualsSTR to EqualsSTR.java

---
+++ commands/EqualsSTR.java
@@ -0,0 +1,16 @@
+public class EqualsSTR {
+	
+    public static void main(String[] args) {
+        String aa = "e";
+        String aaa = "e";
+        Boolean x;
+        if (aa.equals(aaa)){
+            x = true;
+        }
+        else {
+            x = false;
+        }
+        System.out.println(x);
+    }
+}
+//output x = true
