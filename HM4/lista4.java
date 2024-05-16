c161b72f73664878f88fcb253635d95fbb85bd6b
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-05-16 09:34:39 -0300

    Update lista4.java

--- HM4/lista4.java
+++ HM4/lista4.java
@@ -3,12 +3,26 @@ import java.util.*;
 public class exercicio1 {
 	public static void main(String[] args) {
 		int[] numeros = {1, 2, 3, 4, 5};
-		int[] numeros3 = {1, 2, 3, 4, 5};
 		for (int i = 0; i < numeros.length; i++){
 		    int x;
 		    x = numeros[i] * 3;
 		    System.out.println("A = " + numeros[i]);
 		    System.out.println("B = " + x);
+		    System.out.println(" ");
+		}
+		System.out.println(".");
+	}
+}
+
+import java.util.*;
+
+public class exercicio2 {
+	public static void main(String[] args) {
+		int[] Numeros = {3, 4, 5, 6, 7, 8};
+		int[] FatorialNumeros = {6, 24, 120, 720, 5040, 40320};
+		for (int i = 0; i < Numeros.length; i++){
+		    System.out.println("A = " + Numeros[i]);
+		    System.out.println("B = " + Numeros[i] + "! = " + FatorialNumeros[i]);
 		    System.out.println(" ");
 		}
 		System.out.println(".");
