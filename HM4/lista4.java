c1780a4c3dc1b624c3d96cf79e8fdcd524765f14
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-05-16 10:09:24 -0300

    Update lista4.java

--- HM4/lista4.java
+++ HM4/lista4.java
@@ -70,3 +70,49 @@ public class exercicio4
 		System.out.println(".");
 	}
 }
+
+import java.util.*;
+
+public class exercicio5 {
+	public static void main(String[] args) {
+		int[] A = new int[20];
+		int[] B = new int[30];
+		int[] C = new int[50];
+		
+		Random rand = new Random();
+		
+		for (int i = 0; i < A.length; i++){
+		    int x;
+		    x = rand.nextInt(500);
+		    A[i] = x;
+		}
+		for (int i = 0; i < B.length; i++){
+		    int x;
+		    x = rand.nextInt(500);
+		    B[i] = x;
+		}
+		System.out.println("Arrays criados! (A, B)");
+		System.out.println(" ");
+		System.out.println(".");
+		System.out.println("..");
+		System.out.println("...");
+		
+		for (int i = 0; i < A.length; i++){
+		    C[i] = A[i];
+		}
+		for (int i = 0; i < B.length; i++){
+		    C[i + 20] = B[i];
+		}
+		System.out.println("Arrays fundidos! (C = A + B)");
+		System.out.println(" ");
+		System.out.println(".");
+		System.out.println("..");
+		System.out.println("...");
+		System.out.println("Imprimindo Array: C!");
+		for (int i = 0; i < C.length; i++){
+		    System.out.println("C[" + i + "]= " + C[i]);
+		    System.out.println(" ");
+		}
+		System.out.println(".");
+	}
+}
