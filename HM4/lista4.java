77792b52f94dd054a5817eb08c103744b9cefa99
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-05-16 09:28:29 -0300

    Update lista4.java

--- HM4/lista4.java
+++ HM4/lista4.java
@@ -1,1 +1,16 @@
+import java.util.*;
 
+public class exercicio1 {
+	public static void main(String[] args) {
+		int[] numeros = {1, 2, 3, 4, 5};
+		int[] numeros3 = {1, 2, 3, 4, 5};
+		for (int i = 0; i < numeros.length; i++){
+		    int x;
+		    x = numeros[i] * 3;
+		    System.out.println("A = " + numeros[i]);
+		    System.out.println("B = " + x);
+		    System.out.println(" ");
+		}
+		System.out.println(".");
+	}
+}
