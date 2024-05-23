e1607cb95566263438e1e9c7bc3627aac7163d55
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-05-23 09:20:28 -0300

    Update CreateMatriz.java

--- commands/CreateMatriz.java
+++ commands/CreateMatriz.java
@@ -1,17 +1,17 @@
 public class Main {
-  public static double[][] createMatriz(int linhas, int colunas) {
-        double[][] arraynome = new double[linhas][colunas];
-        return arraynome;
-  }
+	public static double[][] createMatriz(int linhas, int colunas) {
+        	double[][] arraynome = new double[linhas][colunas];
+        	return arraynome;
+  	}
 	public static void main(String[] args) {
-        int L = 3, C = 3;
-        double[][] x = createMatriz(L, C);
-        System.out.println(x);
-        for (int i = 0; i < L; i++) {
-            for (int j = 0; j < C; j++){
-                double N = x[i][j];
-                System.out.printf(N + " ");
-            }System.out.println("\n");
-        }
+        	int L = 3, C = 3;
+        	double[][] x = createMatriz(L, C);
+        	System.out.println(x);
+        	for (int i = 0; i < L; i++) {
+            		for (int j = 0; j < C; j++){
+	                	double N = x[i][j];
+                			System.out.printf(N + " ");
+		        }System.out.println("\n");
+        	}
 	}
 }
