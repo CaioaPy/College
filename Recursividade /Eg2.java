313d52ad1e7d56e5c7ca1a03b0109c06b3f2b2af
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-08-14 11:12:58 -0300

    Update Eg2.java

--- Recursividade /Eg2.java
+++ Recursividade /Eg2.java
@@ -1,1 +1,20 @@
-
+import java.util.Scanner;
+public class Main {
+    public static int asa(int numero){
+        if (numero == 0){
+            return 0;
+        }
+        else {
+            System.err.println(numero);
+            return (numero % 10) + asa(numero / 10);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int y = scanner.nextInt();
+        int x = asa(y);
+        System.out.println(x);
+        
+	}
+    
+}
