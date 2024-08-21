a40114ffd0b7dd2e8af01f01d63a56d2f9ab330a
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-08-21 09:04:18 -0300

    Update Array.java

--- commands/Java Arrays/Array.java
+++ commands/Java Arrays/Array.java
@@ -1,1 +1,8 @@
-
+import java.util.Arrays;
+public class Main {
+	public static void main(String[] args) {
+	    int[] aa = new int[5];
+	    Arrays.fill(aa, 6);
+	    System.out.println(Arrays.toString(aa));
+	}
+}
