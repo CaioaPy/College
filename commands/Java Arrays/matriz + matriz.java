4de17147b80debc9c280e96422c292980efacc5b
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-08-21 09:04:46 -0300

    Update matriz + matriz.java

--- commands/Java Arrays/matriz + matriz.java
+++ commands/Java Arrays/matriz + matriz.java
@@ -1,1 +1,26 @@
-
+public class Main {
+	public static void main(String[] args) {
+	    int[][] matriz1 = {
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9}
+                        };
+	    int[][] matriz2 = {
+                        {9, 8, 7},
+                        {6, 5, 4},
+                        {3, 2, 1}
+                        };
+        int[][] matriz3 = new int[3][3];
+        for (int i = 0; i < matriz1.length; i++){
+            for (int j = 0; j < matriz1.length; j++){
+                matriz3[i][j] = matriz1[i][j] + matriz2[i][j];
+            }
+        }
+        for (int i = 0; i < matriz3.length; i++){
+            for (int j = 0; j < matriz3.length; j++){
+                System.out.print(matriz3[i][j] + " ");
+            }
+            System.out.println("");
+        }    
+	}
+}
