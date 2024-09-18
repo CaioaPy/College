69b97f9935694cd13ee292d1bf5084d54372ef82
yatomikazuchi <caioevilabner@gmail.com>
2024-09-18 20:53:01 -0300

    updated

---
+++ TesteJava/POO.java
@@ -0,0 +1,17 @@
+public class POO {
+    public static void main(String[] args) {
+        System.out.print("aa");
+
+    }
+    public class Pessoa {
+        private String nome;
+        private int idade;
+        public String getNome() {
+        return nome;
+        }
+        public void setIdade(int novaIdade) {
+        idade = novaIdade;
+        }
+        }
+        
+}
--- TesteJava/TesteJava/POO.java
+++
@@ -1,6 +0,0 @@
-package TesteJava;
-
-
-public static void main() {
-
-}
