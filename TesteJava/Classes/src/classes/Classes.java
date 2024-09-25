df2a6633f03eb2340b2c0aee4d513ed2d8e83c3e
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-09-25 08:38:11 -0300

    Class Carro construtor e atributos

---
+++ TesteJava/Classes/src/classes/Classes.java
@@ -0,0 +1,8 @@
+package classes;
+
+public class Classes {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro("honda", 121, "cinza");
+        carro1.mostrarInfo();
+    }
+}
