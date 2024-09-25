343205a684d3c3caffc60943b3a49abfec825a37
Caio <159095162+CaioaPy@users.noreply.github.com>
2024-09-25 08:49:08 -0300

    Classe utilitária Calculadora

---
+++ TesteJava/Classes/src/classes/Calculadora.java
@@ -0,0 +1,25 @@
+package classes;
+
+public class Calculadora {
+    
+    public void soma(double numero1, double numero2){
+        double numerofinal;
+        numerofinal = numero1 + numero2;
+        System.out.println(numerofinal);
+    }
+    public void subtracao(double numero1, double numero2){
+        double numerofinal;
+        numerofinal = numero1 - numero2;
+        System.out.println(numerofinal);
+    }
+    public void multiplicacao(double numero1, double numero2){
+        double numerofinal;
+        numerofinal = numero1 * numero2;
+        System.out.println(numerofinal);
+    }
+    public void divisao(double numero1, double numero2){
+        double numerofinal;
+        numerofinal = numero1 / numero2;
+        System.out.println(numerofinal);
+    }
+}
--- TesteJava/Classes/src/classes/Classes.java
+++ TesteJava/Classes/src/classes/Classes.java
@@ -6,5 +6,10 @@ public class Classes {
         carro1.mostrarInfo();
         Livro livro1 = new Livro("o fim", "osamu", 391);
         livro1.exibirInfo();
+        Calculadora calculadora = new Calculadora();
+        calculadora.soma(3, 4);
+        calculadora.subtracao(3, 4);
+        calculadora.multiplicacao(3, 4);
+        calculadora.divisao(3, 4);
     }
 }
