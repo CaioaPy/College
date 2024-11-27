13c53a04a7df40f27e88e0e7e19acd2f4b601fb1
yatomikazuchi <caioevilabner@gmail.com>
2024-11-27 00:18:41 -0300

    codigo prontado

--- 2sem/busca linear.java
+++ 2sem/busca linear.java
@@ -1,3 +1,17 @@
-public class busca linear {
-    
+public class Main {
+    public static int busca(int target, int[] array) {
+        for (int i = 0; i < array.length; i++){
+            if (i == target){
+                System.out.println(i);
+                return i;
+            }
+        }
+        return -1;
+    }        
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6};
+        int target = 2;
+        int x = busca(target, array);
+        System.out.println(x);
+    }
 }
