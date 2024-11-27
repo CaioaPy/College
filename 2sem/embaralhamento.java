52353203c7868705275b1e2f48a56bed3ade1360
yatomikazuchi <caioevilabner@gmail.com>
2024-11-26 23:36:47 -0300

    fisher yates

--- 2sem/embaralhamento.java
+++ 2sem/embaralhamento.java
@@ -1,8 +1,9 @@
 import java.util.Random;
 public static void main(String [ ] args) {
-    String[] array = {"1","2","3","4","5"};
-    
-    for (int i = array.length; i < 0; i--){
-        int j = 
+    int[] array = {1,2,3,4,5};
+    Random random = new Random();
+    for (int i = array.length - 1; i > 0; i--) {
+    int j = random.nextInt(i + 1);
+    swap(array, i, j);
     }
 }
