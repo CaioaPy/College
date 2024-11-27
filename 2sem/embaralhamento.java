f297874a73c32cb3911abd56ec47333b6a26cf0a
yatomikazuchi <caioevilabner@gmail.com>
2024-11-26 23:23:23 -0300

    new file

---
+++ 2sem/embaralhamento.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+public static void main(String [ ] args) {
+    String[] array = {"1","2","3","4","5"};
+    
+    for (int i = array.length; i < 0; i--){
+        int j = 
+    }
+}
